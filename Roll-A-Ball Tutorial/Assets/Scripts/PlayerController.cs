32a917fcb3c47ef1fe8638756ed840e0b9d8326d
Ben Mulligan <benpmulligan@gmail.com>
2017-01-23 23:32:03 -0500

    Add text UI

--- Roll-A-Ball Tutorial/Assets/Scripts/PlayerController.cs
+++ Roll-A-Ball Tutorial/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
+	private int count;
+
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	void Start () {
-
 		this.rb = GetComponent<Rigidbody> ();
+		this.count = 0;
+		this.winText.text = "";
+		this.UpdateCountText ();
 	}
 		
 	void FixedUpdate () {
@@ -18,14 +25,21 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (h, 0, v);
 
-		rb.AddForce (movement * speed);
+		this.rb.AddForce (movement * speed);
 	}
 
 	void OnTriggerEnter (Collider other) {
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
+			this.count++;
+			this.UpdateCountText ();
 		}
 	}
-
-
+		
+	void UpdateCountText() {
+		this.countText.text = "Count: " + this.count.ToString();
+		if (this.count >= 12) {
+			this.winText.text = "Ya done it!";
+		}
+	}
 }
