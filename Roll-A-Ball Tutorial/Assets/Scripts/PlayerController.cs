b87cbd83b01e159dc206f362a3ed710eeb487519
Ben Mulligan <benpmulligan@gmail.com>
2017-01-23 20:59:58 -0500

    Add basic movement

---
+++ Roll-A-Ball Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+
+	void Start () {
+
+		this.rb = GetComponent<Rigidbody> ();
+	}
+		
+	void FixedUpdate () {
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3 (h, 0, v);
+
+		rb.AddForce (movement * speed);
+	}
+}
